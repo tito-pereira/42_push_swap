f32259db8ea02f48cb01bf8df9e7b5397f6cfe8f
Tito Alexandre Barbosa Pereira <tibarbos@c1r13s11.42porto.com>
2023-09-11 18:43:12 +0100

    s

---
+++ algorithm.c
@@ -0,0 +1,13 @@
+#include "push_swap.h"
+
+//mergesort
+//dividir sempre em dois arrays em metades
+//recursivo, ou seja, chama a propria funcao
+
+//quicksort
+//escolhe um pivo, menores p esquerda, maiores p direita, pivo fica sempre no sitio correto
+//cria sub arrays na esquerda e direita
+//recursivo, chama a propria funcao
+
+//ou faco insertion que e mais intuitivo e natural, mas com ma complexidade
+//ou faco radix/merge/quick, com adaptacoes
