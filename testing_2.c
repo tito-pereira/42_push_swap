ee19623aeb1221c0ddcc0c29fcd85e4d2aae0624
Tito Alexandre Barbosa Pereira <tibarbos@student.42porto.com>
2023-09-14 11:45:08 +0100

    test 2 first part

--- testing_2.c
+++ testing_2.c
@@ -13,50 +13,49 @@ typedef	struct	stuff {
 	struct stuff	*next;
 }	list;
 
-//criar dois (int *) ja completos
-int	array[] = {10, 4, 6, 1, 8}
-int	sorted[] = {4, 1, 2, 0, 3}
+int	array[] = {10, 4, 6, 1, 8};
+int	sorted[] = {4, 1, 2, 0, 3};
 
 //----------create / add to stack--------------------
 
 list	*create_node(list *stacka, int array, int order) {
-    list	new;
+    list	*new;
     new = malloc(sizeof(list));
-    if (!new)
-        return;
-    new.data = array;
-	new.index = order;
-    new.next = NULL;
-	stacka = &new;
-	return (stacka);
+    new->data = array;
+	new->index = order;
+    new->next = NULL;
+	return (new);
 }
 
-list	*add_node(list *stacka, int array, int order) {
+list	*lst_add_front(list *stacka, int array, int order) {
 	list	*new;
+	list	*tmp;
+	tmp = stacka;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
 	new = create_node(new, array, order);
-	if (!new)
-        return;
-	new.next = &stacka;
-	return (new);
+	tmp->next = new;
+	return (stacka);
 }
 
 list	*create_stack(list *stacka, int *array, int *sorted) {
 	int	i;
-	i = f_arrlen(array); //para comecar a adicionar a stack ao contrario, lst_add_back
-	if (stacka == NULL)
+	i = 0;
+	if (stacka == NULL) {
 		stacka = create_node(stacka, array[i], sorted[i]);
-	while (i > 0) {
-		stacka = add_node(stacka, array[i], sorted[i]);
-		i--;
+		i++;
+	}
+	while (array[i]) {
+		stacka = lst_add_front(stacka, array[i], sorted[i]);
+		i++;
 	}
 	return (stacka); 
 }
-//ou posso adicionar direto com um list_add_front
 
 //--------------stack algorithm----------------------
 
 //rotate, swap, push
-void	list_algo(list *stacka, list *stackb) {
+/*void	list_algo(list *stacka, list *stackb) {
 	for (int i = 0; (stacka->index != 0) && (stacka != NULL); i++) {
 		while (stacka->index != i) {
 			lst_r(stacka);
@@ -71,23 +70,64 @@ void	list_algo(list *stacka, list *stackb) {
 		lst_p(b->a);
 		printf("pa\n");
 	}
-}
+}*/
 
 //----------------main central-----------------------
+
+void	print_stack(list *stack) {
+	int	i = 1;
+	while (stack->next != NULL) {
+		printf("element:%d;\ndata:%d;\nindex:%d;\n-----\n", i, stack->data, stack->index);
+		stack = stack->next;
+		i++;
+	}
+	if (stack->next == NULL)
+		printf("element:%d;\ndata:%d;\nindex:%d;\n-----\n", i, stack->data, stack->index);
+}
 
 int	main() {
 	list	*stacka;
 	list	*stackb;
-	//int	array[] = {10, 4, 6, 1, 8}
-	//int	sorted[] = {4, 1, 2, 0, 3}
+	stacka = NULL;
 	stackb = NULL;
 	stacka = create_stack(stacka, array, sorted);
-	list_algo(stacka, stackb);
+	print_stack(stacka);
+	//ate aqui funciona top
 }
-
-//vou testar outra parte qualquer aqui
-//talvez a parte de criar listas em si, e das funções de listas
 
 //ja sei, a minha nova duvida era - eu nem sequer preciso de passar a data em si para dentro da stack,
 //da estrutura, da lista, etc. eu so preciso de passar o indice, ou passo a data na mesma para dar display?
 //eu acho que o push swap so da as instrucoes. pelo sim pelo nao, passar a data
+
+/*
+list	*create_node(list *stacka, int array, int order) {
+    list	new;
+    new = (list)malloc(sizeof(list));
+    new.data = array;
+	new.index = order;
+    new.next = NULL;
+	stacka = &new;
+	return (new);
+}
+list	*lst_add_back(list *stacka, int array, int order) {
+	list	*new;
+	new = create_node(new, array, order);
+	if (!new)
+        return;
+	new.next = &stacka;
+	return (new);
+}
+list	*create_stack(list *stacka, int *array, int *sorted) {
+	int	i;
+	i = f_arrlen(array);
+	if (stacka == NULL) {
+		stacka = create_node(stacka, array[i], sorted[i]);
+		i--;
+	}
+	while (i >= 0) {
+		stacka = add_node(stacka, array[i], sorted[i]);
+		i--;
+	}
+	return (stacka); 
+}
+*/
