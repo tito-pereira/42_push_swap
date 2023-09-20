e75873a6dda2942bd3e78461cc4dc5c307371df4
Tito Alexandre Barbosa Pereira <tibarbos@student.42porto.com>
2023-09-20 17:12:08 +0100

    m

--- version 1/big_2.c
+++ version 1/big_2.c
@@ -7,4 +7,15 @@ versao 2-
 
 teoricamente, quanto mais funcoes simultaneas eu usar melhor, mas mais dificil e c calculos intermedios
 e depois testar e corrigir erros etc
+uso push, swaps
+
+calculos e usar varias funcoes ao mm tempo, swaps, rotates, push, doubles, etc
+
+puta de dor de cabeca
+e preciso milhoes de condicoes e calculos intermedios
+mandar em chunks
+
+ao receber na stack b, posso fazer ja swap e 
 */
+
+
